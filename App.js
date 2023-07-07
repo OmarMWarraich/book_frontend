1bfe9354a811666d6c17542ea0c42eca083778b9
OmarMWarraich <earthling986@gmail.com>
2023-07-08 01:44:05 +0500

    feat: Stack Screen

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import BooksScreen from './screens/BooksScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <BooksScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="자유톡" 
+            component={BooksScreen} 
+            options={{ 
+              headerTitleAlign: 'center',
+              headerShadowVisible: false,
+             }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
